06f8d433219f3e002fb1502c33d81377b97fda3b
praveen-1995 <praveenkumarthipparam@gmail.com>
2022-04-27 18:14:58 -0500

    05-Menu

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems, categories }) => {
+  return (
+    <main>
+      <div className="btn-container">
+        {categories.map((category, index) => {
+          return (
+            <button
+              type="button"
+              key={index}
+              className="filter-btn"
+              onClick={() => {
+                return filterItems(category);
+              }}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    </main>
+  );
 };
 
 export default Categories;
