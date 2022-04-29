68773b8d16f67362c3649d35f174afcb4b7f7da7
praveen-1995 <praveenkumarthipparam@gmail.com>
2022-04-28 23:09:29 -0500

    07-Slider

--- 07-slider/setup/src/AlternativeApproach.js
+++ 07-slider/setup/src/AlternativeApproach.js
@@ -6,19 +6,35 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, people]);
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1;
+      if (index > people.length - 1) {
+        return 0;
+      }
+      return index;
+    });
+  };
+
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex - 1;
+      if (index < 0) {
+        return people.length - 1;
+      }
+      return 0;
+    });
+  };
 
   useEffect(() => {
     const slider = setInterval(() => {
-      setIndex(index + 1);
+      setIndex((oldIndex) => {
+        const index = oldIndex + 1;
+        if (index > people.length - 1) {
+          return 0;
+        }
+        return index;
+      });
     }, 3000);
     return () => {
       clearInterval(slider);
@@ -60,20 +76,10 @@ function App() {
             </article>
           );
         })}
-        <button
-          className="prev"
-          onClick={() => {
-            setIndex(index - 1);
-          }}
-        >
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft></FiChevronLeft>
         </button>
-        <button
-          className="next"
-          onClick={() => {
-            setIndex(index + 1);
-          }}
-        >
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight></FiChevronRight>
         </button>
       </div>
