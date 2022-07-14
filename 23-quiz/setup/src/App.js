9d037364de122f7d59674189081c3c82b4dbebd6
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-07-14 00:51:48 -0500

    23-Quiz

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -1,11 +1,69 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
-import SetupForm from './SetupForm'
-import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from "./SetupForm";
+import Loading from "./Loading";
+import Modal from "./Modal";
 function App() {
-  return <h2>quiz starter</h2>
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext();
+
+  if (waiting) {
+    return <SetupForm></SetupForm>;
+  }
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  const { correct_answer, incorrect_answers, question } = questions[index];
+  // const answers = [...incorrect_answers, correct_answer];
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
+
+  return (
+    <main>
+      <Modal></Modal>
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers : {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }}></h2>
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  onClick={() => {
+                    checkAnswer(correct_answer === answer);
+                  }}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                ></button>
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question" onClick={nextQuestion}>
+          next question
+        </button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
